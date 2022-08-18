5a3b4b3c8fbcb36f04184799b7d6d9967660c115
Geba <igeba@inbox.ru>
2022-08-18 21:45:51 +0500

    Создан игнор

---
+++ Example2_Hello_user/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
